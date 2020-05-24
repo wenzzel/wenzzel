676e3b29e76f78364228638621d52375b3f804a7
wenzzel <102515wenzzel@gmail.com>
2020-05-24 23:06:09 +0900

    2020/05/24
    http://taikikaihou909.blog.fc2.com/blog-entry-5968.html?sp

--- python-master/acwong00/0000/test.py
+++ python-master/acwong00/0000/test.py
@@ -1,11 +1,21 @@
+import sys,os
 from PIL import Image, ImageDraw, ImageFont
 
+os.chdir("C:\\work\\python\\wenzzel\\python-master\\acwong00\\0000")
 original = Image.open("acwong.jpg")
 
 fnt = ImageFont.truetype("msyh.ttf", 80)
 
 d = ImageDraw.Draw(original)
 
-d.text((200, 0), "6", font=fnt, fill=(255,0,0,255))
+d.text((200, 0), "8", font=fnt, fill=(255,255,0,255))
 
-original.save("finnal.jpg")
+original.save("finnal1.jpg")
+
+img = Image.open("finnal.jpg")
+
+#グレースケールに変換
+gray_img = img.convert("L")
+
+#save
+gray_img.save("gray_image.jpg")
